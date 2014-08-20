f8e816936188b893e730fd26629e9ee7c1ebf41f
Tom Lee <tomlee.tomlee@gmail.com>
2014-08-20 10:20:10 -0700

    Completed loops

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -12,7 +12,36 @@
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @"";
+    if (number == otherNumber) {
+        return [NSString stringWithFormat:@"%d", (NSInteger) number];
+    }
+    
+    
+    else if (number < otherNumber){
+                NSMutableString *newNumberString = [NSMutableString stringWithFormat:@"%d", (NSInteger) number];
+        
+            while (number != otherNumber) {
+                number++;
+                NSMutableString *newNumberIncrement = [NSMutableString stringWithFormat:@"%d", (NSInteger) number];
+                [newNumberString appendString: newNumberIncrement];
+                   }
+        
+        return newNumberString;
+    }
+    
+    
+    else if (number > otherNumber){
+        NSMutableString *newNumberString = [NSMutableString stringWithFormat:@"%d", (NSInteger) otherNumber];
+        
+        while (number !=otherNumber) {
+            otherNumber++;
+            NSMutableString *newNumberIncrement = [NSMutableString stringWithFormat:@"%d", (NSInteger) otherNumber];
+            [newNumberString appendString: newNumberIncrement];
+        }
+        return newNumberString;
+        }
+
+    return nil;
 }
 
 @end
