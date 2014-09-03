4a4792af117cb977d9536a86b053b9e945e73ee1
Tom Lee <tomlee.tomlee@gmail.com>
2014-09-03 10:52:42 -0700

    Completed simple data types and objects

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -12,17 +12,34 @@
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
     /* WORK HERE */
-    return @0;
+    NSInteger value = [number integerValue];
+    value = value * 2;
+    return @(value);
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @[];
+    
+    NSMutableArray *returnArray = [NSMutableArray array];
+    for (NSInteger i = number; i <= otherNumber; i++) {
+        [returnArray addObject:@(i)];
+    }
+    return returnArray;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
     /* WORK HERE */
-    return 0;
+    NSInteger lowestNumber = 1000000;
+
+    for ( NSNumber *num in arrayOfNumbers) {
+        NSInteger integervalue = [num integerValue];
+        
+        if ( integervalue < lowestNumber) {
+            lowestNumber = integervalue;
+        }
+    }
+    
+    return lowestNumber;
 }
 
 @end
