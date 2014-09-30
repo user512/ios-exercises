b0bcdab197184e2e4908bd9f9114a471b5a79293
Tom Lee <tomlee.tomlee@gmail.com>
2014-09-30 09:41:08 +0800

    SwiftExercises.playground

--- SwiftExercises.playground/section-1.swift
+++ SwiftExercises.playground/section-1.swift
@@ -8,7 +8,8 @@ Strings
 
 func favoriteCheeseStringWithCheese(cheese: String) -> String {
     // WORK HERE
-    return cheese
+    let sentence = "My favorite cheese is \(cheese)."
+    return sentence
 }
 
 let fullSentence = favoriteCheeseStringWithCheese("cheddar")
@@ -23,10 +24,19 @@ Arrays & Dictionaries
 let numberArray = [1, 2, 3, 4]
 // Add 5 to this array
 // WORK HERE
+var mutableArray = numberArray
+mutableArray.append(5)
+
+mutableArray
+
 
 let numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
 // Add 5 : "five" to this dictionary
 // WORK HERE
+var mutableDictionary = numberDictionary
+mutableDictionary[5] = "five"
+
+mutableDictionary
 
 /*
 
@@ -36,9 +46,16 @@ Loops
 
 // Use a closed range loop to print 1 - 10, inclusively
 // WORK HERE
+for i in (1...10){
+    println(i)
+}
 
 // Use a half-closed range loop to print 1 - 10, inclusively
 // WORK HERE
+for i in (1..<10) {
+    println(i)
+}
+
 
 let worf = [
     "name": "Worf",
@@ -53,18 +70,42 @@ let picard = [
     "information": "Captain of the USS Enterprise",
     "favorite drink": "tea, Earl Grey, hot"]
 
-let characters = [worf, picard]
+picard["favorite drink"]
 
-func favoriteDrinksArrayForCharacters(characters:Array<Dictionary<String, String>>) -> Array<String> {
+let characters = [worf, picard]
+var drinks: [String] = ["Coke", "Sprite"]
+for aDrink in drinks {
+    println(aDrink)
+}
+
+func favoriteDrinksArrayForCharacters(characters:Array< Dictionary<String, String> >) -> Array<String> {
     // return an array of favorite drinks, like ["prune juice", "tea, Earl Grey, hot"]
     // WORK HERE
-    return []
+    var drinks: [String]
+    for /** 1. element name **/(dictionary) in characters {
+        // 2. Get favorite drink
+        drinks.append(dictionary["favroite drink"]!)
+    
+        // 3. Put favorite drink into drinks
+    }
+    return drinks
+    
 }
 
 let favoriteDrinks = favoriteDrinksArrayForCharacters(characters)
 
 favoriteDrinks
 
+let ronWeasley = ["name" : "ron", "hair color" : "red"]
+for (characteristic, descriptionOfCharacteristic) in ronWeasley {
+    println("His \(characteristic) is \(descriptionOfCharacteristic).")
+}
+
+// Output:
+// His name is ron.
+// His hair color is red.
+
+/* (The output's order is similarly arbitrary.) */
 /*
 
 Functions
@@ -76,6 +117,8 @@ Functions
 let strings = ["milk", "eggs", "bread", "challah"]
 
 // WORK HERE - make your function and pass `strings` in
+
+let actualOutput = join(";", strings)
 
 let expectedOutput = "milk;eggs;bread;challah"
 
@@ -89,3 +132,12 @@ let cerealArray = ["Golden Grahams", "Cheerios", "Trix", "Cap'n Crunch OOPS! All
 
 // Use a closure to sort this array alphabetically
 // WORK HERE
+
+func alphabetically(cereal1: String, cereal2: String) -> Bool {
+    return cereal1 < cereal2
+}
+
+var sortedArray = sorted(cerealArray, alphabetically)
+
+
+
